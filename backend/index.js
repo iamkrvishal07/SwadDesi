ba859d2df65a31f0269c054dbe4a77631d8fd292
Vishal Kumar <iamkrvishal07@gmail.com>
2025-07-25 09:11:53 +0530

    updations of CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,14 @@ const app = express();
 const PORT = 3000 || process.env.PORT ;
 
 app.use(express.json()); 
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
+
 app.use("/",userRoutes)
 app.use("/recipe", recipeRoutes);
 
