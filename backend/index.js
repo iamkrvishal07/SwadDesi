36ebe2804bc97473ed3ae2d71d29fa5493338c66
Vishal Kumar <141834992+iamkrvishal07@users.noreply.github.com>
2025-07-25 09:44:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/user.router.js")
 
 
 const app = express();
-const PORT = 3000 || process.env.PORT ;
+const PORT = process.env.PORT || 3000 ;
 
 app.use(express.json()); 
 // app.use(cors())
