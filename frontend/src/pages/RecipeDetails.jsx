9b47339632e02b2dfdd9d7ebfc55c83cb936e6cc
Vishal Kumar <141834992+iamkrvishal07@users.noreply.github.com>
2025-07-25 11:27:03 +0530

    Update RecipeDetails.jsx

--- frontend/src/pages/RecipeDetails.jsx
+++ frontend/src/pages/RecipeDetails.jsx
@@ -2,22 +2,39 @@ import profileImg from '../assets/profile.png'
 import { useLoaderData } from 'react-router-dom'
 
 export default function RecipeDetails() {
-    const recipe=useLoaderData()
-    console.log(recipe)
+  const recipe = useLoaderData();
+
+  // Fallback check
+  if (!recipe) {
+    return (
+      <div className="text-center text-red-600 font-semibold mt-10">
+        Recipe Not Found or Something Went Wrong.
+      </div>
+    );
+  }
+
   return (
-   <>
     <div className='outer-container'>
-        <div className='profile'>
-            <img src={profileImg} width="50px" height="50px"></img>
-            <h5>{recipe.email}</h5>
+      <div className='profile'>
+        <img src={profileImg} width="50px" height="50px" alt="Profile" />
+        <h5>{recipe.email}</h5>
+      </div>
+      <h3 className='title'>{recipe.title}</h3>
+      <img src={recipe.CoverImage} width="220px" height="200px" alt="Recipe" />
+      <div className='recipe-details'>
+        <div className='ingredients'>
+          <h4>Ingredients</h4>
+          <ul>
+            {recipe.ingredients?.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
         </div>
-        <h3 className='title'>{recipe.title}</h3>
-        <img src={recipe.CoverImage} width="220px" height="200px"></img>
-        <div className='recipe-details'>
-            <div className='ingredients'><h4>Ingredients</h4><ul>{recipe.ingredients.map(item=>(<li>{item}</li>))}</ul></div>
-            <div className='instructions'><h4>Instructions</h4><span>{recipe.instructions}</span></div>
+        <div className='instructions'>
+          <h4>Instructions</h4>
+          <span>{recipe.instructions}</span>
         </div>
+      </div>
     </div>
-   </>
-  )
+  );
 }
