0923b8297c23d28c01fe8e50990afa2ff3dd81dc
Vishal Kumar <iamkrvishal07@gmail.com>
2025-07-25 08:35:34 +0530

    contoller

--- backend/controller/recipe.controller.js
+++ backend/controller/recipe.controller.js
@@ -1,75 +1,71 @@
-const Recipe = require("../models/recipe.model")
-const multer  = require('multer')
+const Recipe = require("../models/recipe.model");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images')
-    },
-    filename: function (req, file, cb) {
-      const filename = Date.now() + '-' + file.fieldname
-      cb(null, filename)
-    }
-  })
-  
-const upload = multer({ storage: storage })
+// ✅ Import Cloudinary storage
+const multer = require('multer');
+const { storage } = require('../config/cloudinary');
+const upload = multer({ storage }); 
 
-
+// GET all recipes
 const getRecipes = async (req, res) => {
-    const recipes = await Recipe.find()
-    return res.json(recipes)
+    const recipes = await Recipe.find();
+    return res.json(recipes);
 };
 
+//  GET recipe by ID
 const getRecipesById = async (req, res) => {
-    const recipe = await Recipe.findById(req.params.id)
-    res.json(recipe)
-    
+    const recipe = await Recipe.findById(req.params.id);
+    res.json(recipe);
 };
+
+// ADD new recipe with Cloudinary image
 const addRecipe = async (req, res) => {
-    console.log(req.file)
-    const {title , ingredients , instructions , time} = req.body;
-    if(!title || !ingredients || !instructions ){
-        res.json({
-            message : "Required fields can't be empty"
-        })
+    const { title, ingredients, instructions, time } = req.body;
+
+    if (!title || !ingredients || !instructions) {
+        return res.status(400).json({ message: "Required fields can't be empty" });
     }
 
-    const newRecipe=await Recipe.create({
-        title,ingredients,instructions,time,CoverImage:req.file.filename,
-        createdBy:req.user.id
-    })
-    return res.json(newRecipe)
-    
+    const newRecipe = await Recipe.create({
+        title,
+        ingredients,
+        instructions,
+        time,
+        CoverImage: req.file?.path, // Cloudinary URL
+        createdBy: req.user.id,
+    });
+
+    return res.status(201).json(newRecipe);
 };
 
-const editRecipe=async(req,res)=>{
-    const {title,ingredients,instructions,time}=req.body 
-    let recipe=await Recipe.findById(req.params.id)
+//  EDIT existing recipe
+const editRecipe = async (req, res) => {
+    const { title, ingredients, instructions, time } = req.body;
+    let recipe = await Recipe.findById(req.params.id);
 
-    try{
-        if(recipe){
-            let CoverImage=req.file?.filename ? req.file?.filename : recipe.coverImage
-            await Recipe.findByIdAndUpdate(req.params.id,{...req.body,CoverImage},{new:true})
-            res.json({title,ingredients,instructions,time})
-        }
+    if (!recipe) {
+        return res.status(404).json({ message: "Recipe not found" });
     }
-    catch(err){
-        return res.status(404).json({message:err})
+
+    const CoverImage = req.file?.path || recipe.CoverImage;
+
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+        req.params.id,
+        { title, ingredients, instructions, time, CoverImage },
+        { new: true }
+    );
+
+    return res.json(updatedRecipe);
+};
+
+// DELETE recipe
+const deleteRecipe = async (req, res) => {
+    try {
+        await Recipe.deleteOne({ _id: req.params.id });
+        res.json({ status: "ok" });
+    } catch (err) {
+        return res.status(400).json({ message: "error" });
     }
-    
-}
-
-
-
-const deleteRecipe=async(req,res)=>{
-    try{
-        await Recipe.deleteOne({_id:req.params.id})
-        res.json({status:"ok"})
-    }
-    catch(err){
-        return res.status(400).json({message:"error"})
-    }
-}
-
+};
 
 module.exports = {
     getRecipes,
@@ -77,5 +73,5 @@ module.exports = {
     editRecipe,
     deleteRecipe,
     getRecipesById,
-    upload
+    upload, // exported multer Cloudinary upload for use in router
 };
